33207e4b204edd15336f78fc3ddeee2a4c8d7119
raviteja642581 <chendiraviteja@gmail.com>
2014-05-06 14:45:35 -0400

    day 2
    day 2 code

---
+++ TodaysClass/TodaysClass/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  TodaysClass
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 ravi teja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+    
+        
+        int numerator = 1 ;
+        int denominator = 3;
+        NSLog (@"The fraction is %i/%i", numerator, denominator);
+    }
+    return 0;
+}
+
